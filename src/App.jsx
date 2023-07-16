ed72217bcc2544e83aa8d1c3408510d1f3a22025
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-16 21:35:17 +0200

    add mafs

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,122 @@
-import { useState } from 'react';
+import * as React from "react"
+import "mafs/core.css";
+import "mafs/font.css";
 
-function Square({value, onSquareClick}) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
+
+import { Mafs, useStopwatch, Point, useMovablePoint, Plot, Vector, Polygon } from "mafs"
+
 
 export default function App() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const xSpan = 1.75
+  const ySpan = 1.75
+  const initialVelocity = useMovablePoint([0.5, 1.5])
 
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
+  const vectorScale = 4
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  const g = 9.8
+  const xVelocity = initialVelocity.x * vectorScale
+  const yVelocity = initialVelocity.y * vectorScale
+  const velocityAngle = Math.atan(yVelocity / xVelocity)
+  const velocityMag = Math.sqrt(
+    xVelocity ** 2 + yVelocity ** 2
+  )
+  const timeOfFlight =
+    Math.abs(2 * velocityMag * Math.sin(velocityAngle)) / g
+
+  function positionAtTime(t) {
+    return [xVelocity * t, yVelocity * t - 0.5 * g * t ** 2]
   }
+  const [restingX, restingY] = positionAtTime(timeOfFlight)
+
+  const {
+    time: t,
+    start,
+    stop,
+  } = useStopwatch({
+    endTime: timeOfFlight,
+  })
+
+  React.useEffect(() => {
+    stop()
+    // Reset the ball's whenever the resting position changes
+  }, [restingX, restingY, stop])
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      <Mafs
+        viewBox={{
+          x: [1 - xSpan, 1 + xSpan],
+          y: [1 - ySpan, 1 + ySpan],
+        }}
+      >
+        <Polygon
+          points={[
+            [-100, 0],
+            [100, 0],
+            [100, -100],
+            [-100, -100],
+          ]}
+          color="green"
+        />
+
+        <Vector
+          tip={[
+            xVelocity / vectorScale,
+            yVelocity / vectorScale,
+          ]}
+        />
+
+        {yVelocity > 0 && (
+          <>
+            <Plot.Parametric
+              xy={positionAtTime}
+              t={[0, timeOfFlight]}
+              opacity={0.4}
+              style="dashed"
+            />
+            <Point
+              x={restingX}
+              y={restingY}
+              opacity={0.5}
+            />
+          </>
+        )}
+
+        <Point
+          x={positionAtTime(t)[0]}
+          y={positionAtTime(t)[1]}
+        />
+        <text
+          x={10}
+          y={30}
+          fontSize={20}
+          className="transform-to-center"
+          fill="white"
+        >
+          t = {t.toFixed(2)}/
+          {yVelocity > 0 ? timeOfFlight.toFixed(2) : "—"}{" "}
+          seconds
+        </text>
+
+        {initialVelocity.element}
+      </Mafs>
+
+      {/* These classnames are part of the Mafs docs website—they won't work for you. */}
+      <div className="p-4 bg-black border-t border-gray-900 space-x-4">
+        <button
+          className="bg-gray-200 text-black font-bold px-4 py-1 rounded-sm"
+          onClick={start}
+          disabled={yVelocity <= 0}
+        >
+          Start
+        </button>
+        <button
+          className="bg-gray-200 text-black font-bold px-4 py-1 rounded-sm"
+          onClick={stop}
+        >
+          Reset
+        </button>
       </div>
     </>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+  )
 }
